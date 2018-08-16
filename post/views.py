61f8d80fde1c2ced4cb615c1a9518677cec3f811
shanghaipython1803 <3562263781@qq.com>
2018-08-16 19:28:53 +0800

    完成了帖子的查询、分页展示、删除

--- post/views.py
+++ post/views.py
@@ -27,7 +27,9 @@ def edit(request):
         return render(request, 'edit.html', {'post':post})
 
 def delete(request):
-    return render(request, 'delete.html', {})
+    post_id = request.GET.get('post_id')
+    Post.objects.get(pk=post_id).delete()
+    return redirect('/')
 
 def read(request):
     post_id = request.GET.get('post_id')
@@ -35,8 +37,14 @@ def read(request):
     return render(request, 'read.html', {'post':post})
 
 def list(request):
-    posts = Post.objects.all()
-    return render(request, 'list.html', {'posts':posts})
+    page = int(request.GET.get('page', 1))
+    totle = Post.objects.count()
+    per_page = 10
+    pages = totle // 10 + 1
+    start = (page - 1) * per_page
+    end = start + per_page
+    posts = Post.objects.all().order_by('-id')[start:end]  # 惰性加载
+    return render(request, 'list.html', {'posts':posts, 'pages':range(pages)})
 
 def search(request):
     keyword = request.POST.get('keyword')
